e76a485245eb4f2fc0114ed53f0a5c55598d247f
yassineaba-dev <yassineaba.dev@gmail.com>
2025-08-20 15:28:08 +0100

    first commit

--- components/Thumbnail.tsx
+++ components/Thumbnail.tsx
@@ -22,16 +22,13 @@ export const Thumbnail = ({
   return (
     <figure className={cn("thumbnail", className)}>
       <Image
-        src={isImage ? url : getFileIcon(extension, type)}
-        alt="thumbnail"
-        width={100}
-        height={100}
-        className={cn(
-          "size-8 object-contain",
-          imageClassName,
-          isImage && "thumbnail-image",
-        )}
-      />
+  src={isImage ? url : getFileIcon(extension, type)}
+  alt="thumbnail"
+  width={100}
+  height={100}
+  className={cn("size-8 object-contain", imageClassName, isImage && "thumbnail-image")}
+  unoptimized={isImage}
+/>
     </figure>
   );
 };
